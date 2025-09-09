ef7bfc170280a2eef171ac8b46acbd48bec8c9a9
siham binta habib <sihambintahabib@gmail.com>
2025-09-09 22:30:42 +0600

    v

--- code.js
+++ code.js
@@ -2,8 +2,11 @@
 var categoryContainer = document.getElementById("categoryContainer");
 var cardsContainer = document.getElementById("cardsContainer");
 var pricelist = document.getElementById("pricelist");
+var totalprice = document.getElementById("totalprice");
 
 // button click even run
+//price section start=============
+
 var prices = [];
 cardsContainer.addEventListener("click", (e) => {
     // console.log(e.target.parentNode.parentNode.childNodes[1]);
@@ -28,9 +31,10 @@ var handleprices = (e) => {
 
 var showprice = (prices) => {
     pricelist.innerHTML = "";
+    totalprice.innerHTML = '';
+
     prices.forEach(price => {
-        pricelist.innerHTML += `
-            <li class="bg-white p-2 m-1 rounded-sm ">
+        pricelist.innerHTML += `<li class="bg-white p-2 m-1 rounded-sm ">
             <p class="text-lg font-semibold ">${price.title}</p>
             <p class="flex justify-between">
               <span class="font-light text-sm ">${price.price}</span>
@@ -41,7 +45,24 @@ var showprice = (prices) => {
               </button>
             </p>
           </li>  
-            `  })
+            `
+       totalprice.innerHTML += `<span class="font-light text-sm ">${price.price}</span>`
+        var price_converter = Number(a)
+        console.log(a);
+        console.log(price_converter);
+        console.log(typeof(price_converter));
+
+        
+        })
+
+    // prices.forEach(price => {
+    //     // totalprice += price.price;
+        // totalprice.innerHTML += `<span class="font-light text-sm ">${price.price}</span>
+    //         `  })
+
+
+    // totalprice.innerHTML += price
+
 }
 
 var handledeletePrices = (pi) => {
@@ -52,6 +73,9 @@ var handledeletePrices = (pi) => {
     console.log(filteredPrices);
     showprice(prices);
 }
+
+
+//price section end=============
 
 //fetch data - catagories list
 var loadCategory = () => {
@@ -86,38 +110,40 @@ var loadallCategory = () => {
             console.log(err);
         })
 }
-var CardDesing = (plants) => {
-    cardsContainer.innerHTML = "";
-    cardsContainer.innerHTML +=
-        `<div id='cardparent' class="cc ">
-    <div class=' bg-white shadow-2xl overflow-hidden |  flex flex-col gap-0 justify-between border border-gray-200 rounded-lg p-2 m-0  '>
-      <div class='w-full h-[40%]'>
-        <img class=" w-full h-50 | outline outline-transparent rounded-md
-" src="${plant.image}" alt="">
-      </div>
+// var CardDesing = (plants) => {
+//     cardsContainer.innerHTML = "";
+//     cardsContainer.innerHTML +=
+//         `<div id='cardparent' class="cc ">
+//     <div class=' bg-white shadow-2xl overflow-hidden |  flex flex-col gap-0 justify-between border border-gray-200 rounded-lg p-2 m-0  '>
+//       <div class='w-full h-[40%]'>
+//         <img class=" w-full h-50 | outline outline-transparent rounded-md
+// " src="${plant.image}" alt="">
+//       </div>
 
-      <div class='card-text |  w-full p-2 | flex flex-col justify-end  gap-2 '>
-<div id="${plant.id}" class='heading | h-fit'>
+//       <div class='card-text |  w-full p-2 | flex flex-col justify-end  gap-2 '>
+// <div id="${plant.id}" class='heading | h-fit'>
 
-        <button onclick="showModal(${plant.id})" class=" text-1xl font-semibold mb-1 ">${plant.name}</button>
-        <p class=" text-sm font-light ">${plant.description}</p>
-                </div>
+//         <button onclick="showModal(${plant.id})" class=" text-1xl font-semibold mb-1 ">${plant.name}</button>
+//         <p class=" text-sm font-light ">${plant.description}</p>
+//                 </div>
 
-        <div class='flex flex-row justify-between'>
-          <span class="text-sm font-light | outline rounded-lg bg-green-200 outline-transparent w-fit text-center px-3 py-2 " >${plant.category}</span>
+//         <div class='flex flex-row justify-between'>
+//           <span class="text-sm font-light | outline rounded-lg bg-green-200 outline-transparent w-fit text-center px-3 py-2 " >${plant.category}</span>
 
-          <span>${plant.price}</span>
-        </div>
+//           <span>${plant.price}</span>
+//         </div>
 
-<div class=''>
- <button class="outline rounded-lg bg-green-500 outline-transparent w-full text-center  m-auto" type="button">Add to Cart button</button>   
-        </div>
-  </div>
-    </div>
-  </div>
-          `
+// <div class=''>
+//  <button class="outline rounded-lg bg-green-500 outline-transparent w-full text-center  m-auto" type="button">Add to Cart button</button>   
+//         </div>
+//   </div>
+//     </div>
+//   </div>
+//           `
 
-}
+// }
+
+
 showallCategory = (plants) => {
     allButton.innerHTML = "";
     allButton.innerHTML = `<li id="" class="allCategories | px-3 py-3 hover:bg-green-200 bg-green-200 cursor-pointer ">All Catagories</li> `
@@ -151,22 +177,12 @@ showallCategory = (plants) => {
     </div>
   </div>
           `
-
     })
-
-
-
         ;
 
     allButton.addEventListener('click', (e) => {
         cardsContainer.innerHTML = "";
-
-        // alert('Button clicked!');
-        console.log(e);
-        console.log(plants);
-
         plants.forEach(plant => {
-
             cardsContainer.innerHTML += `
         <div id='cardparent' class="cc  ">
     <div class=' bg-white shadow-2xl overflow-hidden |  flex flex-col gap-0 justify-between border border-gray-200 rounded-lg p-2 m-0  '>
@@ -205,7 +221,7 @@ showallCategory = (plants) => {
     });
 
 }
-//name
+//left side-name
 showCategory = (categories) => {
     categories.forEach(cat => {
         categoryContainer.innerHTML += `<li id="${cat.id}" class=" p-3 hover:bg-green-200 cursor-pointer ">${cat.category_name}</li>`
@@ -244,6 +260,38 @@ const plantsByCategories = (plantID) => {
         })
 
 }
+
+
+const showCardsByCategories = (plants) => {
+    cardsContainer.innerHTML = "";
+    plants.forEach(plant => {
+        cardsContainer.innerHTML += `
+        <div id='cardparent' class="cc  ">
+    <div class=' bg-white shadow-2xl overflow-hidden |  flex flex-col gap-0 justify-between border border-gray-200 rounded-lg p-2 m-0  '>
+      <div class='w-full h-[40%]'>
+        <img class=" w-full h-50 | outline outline-transparent rounded-md
+" src="${plant.image}" alt="">
+      </div>
+      <div class='card-text |  w-full p-2 | flex flex-col justify-end  gap-2 '>
+<div id="${plant.id}" class='heading | h-fit'>
+        <button  onclick="showModal(${plant.id})"
+class=" text-1xl font-semibold mb-1 ">${plant.name}</button>
+        <p class=" text-sm font-light ">${plant.description}</p>
+                </div>
+        <div class='flex md:flex-row flex-col-reverse   justify-between'>
+          <span class="text-sm font-light | outline rounded-lg bg-green-200 outline-transparent w-fit text-center px-3 py-2 " >${plant.category}</span>
+          <span>${plant.price}</span>
+        </div>
+<div class=''>
+ <button class="outline rounded-lg bg-green-500 outline-transparent w-full text-center px-1 py-3 m-auto" type="button">Add to Cart button</button>   
+        </div>
+  </div>
+    </div>
+  </div>
+          `    }
+        // hidden
+    )
+}
 //model
 var showModal = (id) => {
     var url = `https://openapi.programming-hero.com/api/plant/${id}`
@@ -260,6 +308,7 @@ var showModal = (id) => {
         })
 
 }
+// model details
 var displaymodelDetails = (data) => {
     console.log(data);
     var text = document.getElementById("text");
@@ -288,39 +337,6 @@ var displaymodelDetails = (data) => {
 
 }
 
-const showCardsByCategories = (plants) => {
-    cardsContainer.innerHTML = "";
-    plants.forEach(plant => {
-        cardsContainer.innerHTML += `
-        <div id='cardparent' class="cc  ">
-    <div class=' bg-white shadow-2xl overflow-hidden |  flex flex-col gap-0 justify-between border border-gray-200 rounded-lg p-2 m-0  '>
-      <div class='w-full h-[40%]'>
-        <img class=" w-full h-50 | outline outline-transparent rounded-md
-" src="${plant.image}" alt="">
-      </div>
-      <div class='card-text |  w-full p-2 | flex flex-col justify-end  gap-2 '>
-<div id="${plant.id}" class='heading | h-fit'>
-        <button  onclick="showModal(${plant.id})"
-class=" text-1xl font-semibold mb-1 ">${plant.name}</button>
-        <p class=" text-sm font-light ">${plant.description}</p>
-                </div>
-        <div class='flex md:flex-row flex-col-reverse   justify-between'>
-          <span class="text-sm font-light | outline rounded-lg bg-green-200 outline-transparent w-fit text-center px-3 py-2 " >${plant.category}</span>
-          <span>${plant.price}</span>
-        </div>
-<div class=''>
- <button class="outline rounded-lg bg-green-500 outline-transparent w-full text-center px-1 py-3 m-auto" type="button">Add to Cart button</button>   
-        </div>
-  </div>
-    </div>
-  </div>
-          `
-
-    }
-        // hidden
-    )
-}
-
 var showloading = (status) => {
     if (status === true) {
         document.getElementById("spinner").classList.remove("hidden")
@@ -331,8 +347,7 @@ var showloading = (status) => {
         document.getElementById("spinner").classList.add("hidden")
     }
 }
-// showloading(true);
-// showloading(false);
+
 
 loadallCategory();
 loadCategory();
